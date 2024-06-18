d92b08bf70c047086fd8a58308198e2773a1694b
MIAIONE <MIAIONE@Yeah.net>
2024-06-18 22:37:48 +0800

    clean others

--- NatTypeTester.Models/NatTypeTesterModelsModule.cs
+++
@@ -1,7 +0,0 @@
-using JetBrains.Annotations;
-using Volo.Abp.Modularity;
-
-namespace NatTypeTester.Models;
-
-[UsedImplicitly]
-public class NatTypeTesterModelsModule : AbpModule;
--- NatTypeTester.ViewModels/ViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-using ReactiveUI;
-using Volo.Abp.DependencyInjection;
-
-namespace NatTypeTester.ViewModels;
-
-public abstract class ViewModelBase : ReactiveObject, ISingletonDependency
-{
-	public IAbpLazyServiceProvider LazyServiceProvider { get; init; } = null!;
-}
